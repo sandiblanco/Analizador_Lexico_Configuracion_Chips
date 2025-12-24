9649069b565d2be84e789c7137ba1149a6383c7a
sandiblanco <s.sandi.1@estudiantec.cr>
2025-12-24 02:52:29 -0600

    doc: documentación

--- PP1_SebastiánSandíBlanco_MelanyBustosCerdas/programa/Main.java
+++ PP1_SebastiánSandíBlanco_MelanyBustosCerdas/programa/Main.java
@@ -1,33 +1,35 @@
+/*
+CURSO: Compiladores e Intérpretes
+PROYECTO #1: Análisis Léxico
+ESTUDIANTES: Sebastián Sandí Blanco y Melany Bustos Cerdas
+*/
+
 import java.io.*;
 import java_cup.runtime.Symbol;
 
-public class main {
+public class Main {
     public static void main(String[] args) throws Exception {
 
         //archivos de texto
-        String archivoFuente = "archivoFuente.txt";
+        String archivoFuente = "programa/archivoFuente.txt";
         String archivoSalida = "tokens_encontrados.txt";
 
         try {
             Reader lectorArchivo = new BufferedReader(
-                    new InputStreamReader(new FileInputStream("entrada.txt"), "UTF-8"));
+                    new InputStreamReader(new FileInputStream(archivoFuente), "UTF-8"));
             Scanner scanner = new Scanner(lectorArchivo); // Clase generada por JFlex
             BufferedWriter writer = new BufferedWriter(
-                    new OutputStreamWriter(new FileOutputStream("tokens_encontrados.txt"), "UTF-8")
+                    new OutputStreamWriter(new FileOutputStream(archivoSalida), "UTF-8")
             );
 
             System.out.println("Iniciando analisis lexico de: " + archivoFuente);
             writer.write("REPORTE DE TOKENS ENCONTRADOS\n");
             writer.write("====================================\n");
+            writer.close();
 
         } catch (UnsupportedEncodingException | FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
-        //Lexer lexer = new Lexer(lectorArchivo);
-
-        //while (lexer.next_token().sym != sym.EOF) {
-        //}
 
         System.out.println("Fin del Análisis léxico");
     }
