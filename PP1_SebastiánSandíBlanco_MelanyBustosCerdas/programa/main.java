f0c26c5a04ab5e742a2b6c792dad6a8ccc24eba4
sandiblanco <s.sandi.1@estudiantec.cr>
2025-12-23 23:00:06 -0600

    chore: cargar los archivos de texto

--- PP1_SebastiánSandíBlanco_MelanyBustosCerdas/programa/main.java
+++ PP1_SebastiánSandíBlanco_MelanyBustosCerdas/programa/main.java
@@ -1,13 +1,34 @@
 import java.io.*;
 
-public class Main {
+import java_cup.runtime.Symbol;
+
+public class main {
     public static void main(String[] args) throws Exception {
-        Reader lectorArchivo = new FileReader("archivoFuente.txt");
 
-        Lexer lexer = new Lexer(lectorArchivo);
+        //archivos de texto
+        String archivoFuente = "archivoFuente.txt";
+        String archivoSalida = "tokens_encontrados.txt";
 
-        while (lexer.next_token().sym != sym.EOF) {
+        try {
+            Reader lectorArchivo = new BufferedReader(
+                    new InputStreamReader(new FileInputStream("entrada.txt"), "UTF-8"));
+            Scanner scanner = new Scanner(lectorArchivo); // Clase generada por JFlex
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream("tokens_encontrados.txt"), "UTF-8")
+            );
+
+            System.out.println("Iniciando analisis lexico de: " + archivoFuente);
+            writer.write("REPORTE DE TOKENS ENCONTRADOS\n");
+            writer.write("====================================\n");
+
+        } catch (UnsupportedEncodingException | FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+
+        //Lexer lexer = new Lexer(lectorArchivo);
+
+        //while (lexer.next_token().sym != sym.EOF) {
+        //}
 
         System.out.println("Fin del Análisis léxico");
     }
